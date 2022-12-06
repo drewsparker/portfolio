f3299d2b25489f9e7c426db1edebe4008378583a
drewsparker <drewsparker@icloud.com>
2022-12-06 17:59:14 -0600

    contact page element updated

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -3,79 +3,8 @@ import emailjs from '@emailjs/browser';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../assets/contact.png";
 import InputGroup from 'react-bootstrap/InputGroup';
-require('dotenv').config()
-
-// function ContactForm() {
-//     const [validated, setValidated] = useState(false);
-
-//     const handleSubmit = (event) => {
-//         const form = event.currentTarget;
-//         if (form.checkValidity() === false) {
-//             event.preventDefault();
-//             event.stopPropagation();
-//         }
-
-//         setValidated(true);
-//     };
-//     return (
-//         <section className="contact-section">
-//             <Container fluid id="contact">
-//                 <Container className="contact-content">
-//                     <Row className="contact-header">
-//                         <Col md={7}>
-//                             <Form noValidate validated={validated} onSubmit={handleSubmit}>
-//                                 <Form.Group className="mb-3" controlId="validationCustomName">
-//                                     <Form.Label>Name</Form.Label>
-//                                     <Form.Control
-//                                         required
-//                                         type="text"
-//                                         placeholder="Enter name"
-//                                     />
-//                                     <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-//                                     <Form.Control.Feedback type="invalid">Please enter name.</Form.Control.Feedback>
-//                                 </Form.Group>
-
-//                                 <Form.Group className="mb-3" controlId="validationEmail">
-//                                     <Form.Label>Email address</Form.Label>
-//                                     <Form.Control
-//                                         required
-//                                         type="email"
-//                                         placeholder="Enter email"
-//                                     />
-//                                     <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-//                                     <Form.Control.Feedback type="invalid">Please enter email.</Form.Control.Feedback>
-//                                     <Form.Text className="text-muted">
-//                                         We'll never share your email with anyone else.
-//                                     </Form.Text>
-//                                 </Form.Group>
-
-//                                 <Form.Group className="mb-3" controlId="validationMessage">
-//                                     <Form.Label>Message</Form.Label>
-//                                     <Form.Control
-//                                         required
-//                                         type="text"
-//                                         placeholder="Enter message"
-//                                     />
-//                                     <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-//                                     <Form.Control.Feedback type="invalid">Please enter message.</Form.Control.Feedback>
-//                                 </Form.Group>
-
-//                                 <Button variant="primary" type="submit">
-//                                     Submit
-//                                 </Button>
-//                             </Form>
-//                         </Col>
-//                     </Row>
-//                 </Container>
-//             </Container>
-//         </section>
-//     );
-// }
-
-// export default ContactForm;
-
-
 
 const validateEmail = (input) => {
     const regex = new RegExp(/^[A-Z0-9_!#$%&'*+/=?`{|}~^-]+(?:\.[A-Z0-9_!#$%&'*+/=?`{|}~^-]+↵)*@[A-Z0-9-]+(?:\.[A-Z0-9-]+)*$/i);
@@ -104,14 +33,10 @@ const Contact = () => {
     const formSubmit = (event) => {
         event.preventDefault();
 
-        // if (!validateEmail(event.email)) {
-        //     console.error('Invalid email')
-        //     return
-        // }
 
         emailjs
             .sendForm(
-                process.env.REACT_SERVICE_ID, process.env.REACT_TEMPLATE_ID, e.target, process.env.REACT_USER_ID
+                'service_o1ygidp', 'template_dqqqude', event.target, 'jre9G4DiRTlXc2Kqc'
             )
             .then(
                 (result) => {
@@ -122,13 +47,14 @@ const Contact = () => {
                 }
             );
         event.target.reset();
+        alert("Thank you for your message! Someone will be in touch with you soon.");
     };
 
     return (
         <section className="contact-section">
             <Container fluid id="contact">
                 <Container className="contact-content">
-                    <Row className="contact-header">
+                    <Row id="contact-row" class='d-flex align-items-center'>
                         <Col md={7}>
                             <Form onSubmit={formSubmit}>
                                 <div className="form-group">
@@ -139,7 +65,7 @@ const Contact = () => {
                                         id="fullname"
                                         name="fullname"
                                         defaultValue={data.fullname}
-                                        onChange={()=>{this._handleChangeEvent(this.state.data);}} 
+                                        onChange={() => { this._handleChangeEvent(this.state.data); }}
                                         // onchange={InputEvent}
                                         placeholder="Enter your full name"
                                     />
@@ -157,7 +83,7 @@ const Contact = () => {
                                     />
                                 </div>
                                 <div class="form-group">
-                                <Form.Group className="my-3" controlId="email">Email</Form.Group>
+                                    <Form.Group className="my-3" controlId="email">Email</Form.Group>
                                     <input
                                         type="email"
                                         class="form-control"
@@ -166,10 +92,10 @@ const Contact = () => {
                                         defaultValue={data.email}
                                         onChange={InputEvent}
                                         placeholder="Enter your email address"
-                                        />
+                                    />
                                 </div>
                                 <div class="form-group">
-                                <Form.Group className="my-3" controlId="message">Message</Form.Group>
+                                    <Form.Group className="my-3" controlId="message">Message</Form.Group>
                                     <input
                                         type="text"
                                         class="form-control"
@@ -181,11 +107,16 @@ const Contact = () => {
                                     />
                                 </div>
                                 <div class="col-12">
-                                    <Button className="btn btn-outline-primary my-3" type="submit">
+                                    <Button id="contact-btn" className="btn btn-outline-primary px-5 mt-5" type="submit">
                                         Submit
                                     </Button>
                                 </div>
                             </Form>
+                        </Col>
+                        <Col md={5} className="contact-image d-flex justify-content-center align-items-center">
+                            <img src={myImg}
+                            className="p-25"
+                            alt="phone" />
                         </Col>
                     </Row>
                 </Container>
